a77d97c22e257d60c71c92d758e3b40dfdb6412e
DZZN-DEV <93328813+DZZN-DEV@users.noreply.github.com>
2023-08-05 19:43:19 +0200

    Update app_constants.dart

--- lib/global/app_constants.dart
+++ lib/global/app_constants.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: constant_identifier_names
 
 class AppConstants {
-  static const BEARER_TOKEN = "";
+  static const BEARER_TOKEN = "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";
   static const CHAT_GPT_MODEL = "gpt-3.5-turbo";
 }
